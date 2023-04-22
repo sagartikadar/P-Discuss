1df4c3eb417a99ecf491a816ab8a8167707b8312
tanishq205 <117253783+sagartikadar@users.noreply.github.com>
2023-04-22 20:21:14 +0530

    Updated 22/04/2023

--- thread_list.php
+++ thread_list.php
@@ -95,8 +95,7 @@
    
   
     
-
-  <?php include 'parts/footer.php'; ?>
+<?php include 'parts/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
